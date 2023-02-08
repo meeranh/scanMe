98cec3a0c26e2bcc1a58fffac6e09854b7bc7ec9
meeranh <themeeranhassan@gmail.com>
2023-02-08 14:50:44 +0530

    Added comments

--- scanMe.sh
+++ scanMe.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# List all common ports to an array
 COMMONPORTS=( 20 21 22 23 25 53 80 110 143 443 993 995 1723 3306 3389 5900 8080 8443 10000 32768)
 
+# Uncomment the following for a more in-depth scan
 # COMMONPORTS=1-65535
 
 [[ -z $1 ]] &&
@@ -12,9 +14,11 @@ Example: ./scanMyPorts.sh 157.240.15.35
 Built by Meeran Hassan
 EndOfMessage
 
+# If an IP address was given
 [[ $1 ]] &&
 echo "Scanning IP: $1"
 
+# Use NetCat to find open/closed ports
 for PORT in ${COMMONPORTS[@]}; do
     echo -n "Port $PORT: "
     nc -z -w 1 $1 $PORT && echo "Open" || echo "Closed"
